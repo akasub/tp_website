8b4bcdd2e84c159813f5cf42ed758a5cdc58ffb8
akasub <akasub23@gmail.com>
2021-03-19 17:42:47 +0100

    module: allWorks

---
+++ src/allWorksList.js
@@ -0,0 +1,8 @@
+export const allWorks = [
+    { number: 210203, type: ['M'], en: 'The world where children live', ko: '아이들이 사는 세상', enTxt: '', enEtc: '', koTxt: '', koEtc: '', imgs: '' },
+    { number: 210202, type: ['M'], en: 'Why do we feel ashamed?', ko: '우리는 왜 부끄러움을 느낄까?' },
+    { number: 210201, type: ['M'], en: 'Waiting for him to come', ko: '그분이 오시기를' },
+    { number: 210101, type: ['E', 'I'], en: 'Fisher\'s Kitchen', ko: '어부의 밥상' },
+    { number: 201202, type: ['G'], en: 'Blind', ko: '블라인드' },
+    { number: 201201, type: ['M'], en: 'Adventskalendar', ko: '산타를 위한 아드벤츠칼렌더' },
+];
